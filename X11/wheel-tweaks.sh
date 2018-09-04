e39af2cdd443e9bf20c8d57eca907ea7390a89c8
Francesco Provino <me@francescoprovino.com>
2018-09-04 12:43:57 +0200

    Fix axes trackpoint

--- X11/wheel-tweaks.sh
+++ X11/wheel-tweaks.sh
@@ -4,5 +4,4 @@ dev="DELL081C:00 044E:121F Mouse"
 xinput set-prop "$dev" "Evdev Wheel Emulation" 1
 xinput set-prop "$dev" "Evdev Wheel Emulation Axes" 6 7 4 5
 xinput set-prop "$dev" "Evdev Wheel Emulation Button" 2
-xinput set-prop "$dev" "Evdev Wheel Emulation Inertia" 23
 xinput set-prop "$dev" "Device Accel Constant Deceleration" 0.6
